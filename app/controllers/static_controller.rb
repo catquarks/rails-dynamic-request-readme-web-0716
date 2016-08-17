d3025b4c1fca0d0eee8bc37ad82db9b97f4d2bc5
Tami <catquarks@gmail.com>
2016-08-17 11:46:11 -0400

    done

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+
+	# we don't even need this method because it requires no logic! the routes.rb file already knows where to go.
+	# def about
+	# end
+
+
+end
